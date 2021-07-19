cdf11d08bca42b30cc38b9074b0e6531fa210365
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-19 19:55:20 -0300

    tests for logical operators with Jest

--- interpreter/index.test.js
+++ interpreter/index.test.js
@@ -21,5 +21,61 @@ describe('Interpreter', () => {
         ).toEqual(1);
       });
     });
+
+    describe('and the code includes MUL', () => {
+      it('multiplies two values', () => {
+        expect(
+          new Interpreter().runCode([PUSH, 2, PUSH, 3, MUL, STOP])
+        ).toEqual(6);
+      });
+    });
+
+    describe('and the code includes DIV', () => {
+      it('divides two values', () => {
+        expect(
+          new Interpreter().runCode([PUSH, 2, PUSH, 3, DIV, STOP])
+        ).toEqual(1.5);
+      });
+    });
+
+    describe('and the code includes LT', () => {
+      it('checks if one values is less than another', () => {
+        expect(new Interpreter().runCode([PUSH, 2, PUSH, 3, LT, STOP])).toEqual(
+          0
+        );
+      });
+    });
+
+    describe('and the code includes GT', () => {
+      it('checks if one values is grater than another', () => {
+        expect(new Interpreter().runCode([PUSH, 2, PUSH, 3, GT, STOP])).toEqual(
+          1
+        );
+      });
+    });
+
+    describe('and the code includes EQ', () => {
+      it('checks if two values are equal', () => {
+        expect(new Interpreter().runCode([PUSH, 2, PUSH, 2, EQ, STOP])).toEqual(
+          1
+        );
+      });
+    });
+
+    describe('and the code includes AND', () => {
+      it('checks if one values is grater than another', () => {
+        expect(
+          new Interpreter().runCode([PUSH, 1, PUSH, 0, AND, STOP])
+        ).toEqual(0);
+      });
+    });
+
+    describe('and the code includes OR', () => {
+      it('checks if one values is grater than another', () => {
+        expect(new Interpreter().runCode([PUSH, 1, PUSH, 0, OR, STOP])).toEqual(
+          1
+        );
+      });
+    });
   });
 });
