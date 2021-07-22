f744b9837cd79516de79796eeb80988a965f1ffd
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-22 19:39:33 -0300

    calculate balance method

--- account/index.js
+++ account/index.js
@@ -23,6 +23,10 @@ class Account {
     const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
     return keyFromPublic.verify(keccakHash(data), signature);
   }
+
+  static calculateBalance({ address, state }) {
+    return state.getAccount({ address }).balance;
+  }
 }
 
 module.exports = Account;
