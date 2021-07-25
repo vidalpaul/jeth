43df1fc25441dec417161256782dc8b08eda81cf
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-25 20:53:31 -0300

    Update index.test.js

--- transaction/index.test.js
+++ transaction/index.test.js
@@ -1,20 +1,22 @@
-const Transaction = require('.');
+const Transaction = require('./index');
 const Account = require('../account');
 const State = require('../store/state');
 
 describe('Transaction', () => {
-  let account,
-    standardTransaction,
-    createAccountTransaction,
-    state,
-    toAccount,
-    miningRewardTransaction;
+  let account;
+  let standardTransaction;
+  let createAccountTransaction;
+  let state;
+  let toAccount;
+  let miningRewardTransaction;
+
   beforeEach(() => {
     account = new Account();
     toAccount = new Account();
     state = new State();
     state.putAccount({ address: account.address, accountData: account });
     state.putAccount({ address: toAccount.address, accountData: toAccount });
+
     standardTransaction = Transaction.createTransaction({
       account,
       to: toAccount.address,
@@ -27,8 +29,9 @@ describe('Transaction', () => {
       beneficiary: account.address,
     });
   });
-  describe('validateStandardTransaction', () => {
-    it('validates a valid a transaction', () => {
+
+  describe('validateStandardTransaction()', () => {
+    it('validates a valid transaction', () => {
       expect(
         Transaction.validateStandardTransaction({
           transaction: standardTransaction,
@@ -36,8 +39,10 @@ describe('Transaction', () => {
         })
       ).resolves;
     });
-    it('does not validate a malformed a transaction', () => {
-      standardTransaction.to = 'different-recipiend';
+
+    it('does not validate a malformed transaction', () => {
+      standardTransaction.to = 'different-recipient';
+
       expect(
         Transaction.validateStandardTransaction({
           transaction: standardTransaction,
@@ -45,6 +50,7 @@ describe('Transaction', () => {
         })
       ).rejects.toMatchObject({ message: /invalid/ });
     });
+
     it('does not validate when the value exceeds the balance', () => {
       standardTransaction = Transaction.createTransaction({
         account,
@@ -59,6 +65,7 @@ describe('Transaction', () => {
         })
       ).rejects.toMatchObject({ message: /exceeds/ });
     });
+
     it('does not validate when the `to` address does not exist', () => {
       standardTransaction = Transaction.createTransaction({
         account,
@@ -73,16 +80,56 @@ describe('Transaction', () => {
         })
       ).rejects.toMatchObject({ message: /does not exist/ });
     });
+
+    it('does not validate when the gasLimit exceeds the balance', () => {
+      standardTransaction = Transaction.createTransaction({
+        account,
+        to: 'foo-recipient',
+        gasLimit: 9001,
+      });
+
+      expect(
+        Transaction.validateStandardTransaction({
+          transaction: standardTransaction,
+          state,
+        })
+      ).rejects.toMatchObject({ message: /exceeds/ });
+    });
+
+    it('does not validate when the gasUsed for the code exceeds the gasLimit', () => {
+      const codeHash = 'foo-codeHash';
+      const code = ['PUSH', 1, 'PUSH', 2, 'ADD', 'STOP'];
+
+      state.putAccount({
+        address: codeHash,
+        accountData: { code, codeHash },
+      });
+
+      standardTransaction = Transaction.createTransaction({
+        account,
+        to: codeHash,
+        gasLimit: 0,
+      });
+
+      expect(
+        Transaction.validateStandardTransaction({
+          transaction: standardTransaction,
+          state,
+        })
+      ).rejects.toMatchObject({ message: /Transaction needs more gas/ });
+    });
   });
-  describe('validateAccountTransaction', () => {
-    it('validates a valid create account transaction', () => {
+
+  describe('validateCreateAccountTransaction()', () => {
+    it('validates a create account transaction', () => {
       expect(
         Transaction.validateCreateAccountTransaction({
           transaction: createAccountTransaction,
         })
       ).resolves;
     });
-    it('does not validate a malformed create account transaction', () => {
+
+    it('does not validate a non create account transaction', () => {
       expect(
         Transaction.validateCreateAccountTransaction({
           transaction: standardTransaction,
@@ -90,6 +137,7 @@ describe('Transaction', () => {
       ).rejects.toMatchObject({ message: /incorrect/ });
     });
   });
+
   describe('validateMiningRewardTransaction', () => {
     it('validates a mining reward transaction', () => {
       expect(
